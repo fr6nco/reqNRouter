588c4254ed9b88a48c0ce577f1576ecb686dfb22
Tomas Boros <tomas.boros@stuba.sk>
2019-02-09 15:58:54 +0100

    retry if was an error when getting the se form controller

--- src/RequestRouterModule/RequestRouter.service.ts
+++ src/RequestRouterModule/RequestRouter.service.ts
@@ -2,7 +2,11 @@ import { ServiceEngine } from './ServiceEngine.service';
 import { Inject, Singleton, AutoWired } from 'typescript-ioc';
 import { ControllerConnectorService } from '../ControllerEndpointConnectorModule/connector.service';
 import { ControllerConnectorStore } from '../ControllerEndpointConnectorModule/store/models';
-import { ServiceEngineStore, RequestRouterStore, session } from './store/models';
+import {
+    ServiceEngineStore,
+    RequestRouterStore,
+    session
+} from './store/models';
 import { HttpEndpointModule } from '../HttpEndpointModule/httpendpoint.service';
 import { httpEvent } from '../HttpEndpointModule/store/models';
 import { LoggerService } from '../LoggerModule/logger.service';
@@ -10,8 +14,8 @@ import { ApiProvider } from '../ApiModule/ApiProviderInterface';
 
 import * as express from 'express';
 import * as config from 'config';
-import { timer } from 'rxjs';
-import { http } from 'winston';
+import { from } from 'rxjs';
+import { take, retryWhen, delay, mergeMap } from 'rxjs/operators';
 
 /**
  * Consider making this a singleton
@@ -44,8 +48,9 @@ export class RequestRouter implements RequestRouterStore, ApiProvider {
      * Domain is returned from controller, which is set
      */
     private register() {
-        this.ccService.registerRR(this.ip, this.port)
-            .then((data:{ name: string; domain: string}) => {
+        this.ccService
+            .registerRR(this.ip, this.port)
+            .then((data: { name: string; domain: string }) => {
                 this.registered = true;
                 this.name = data.name;
                 this.domain = data.domain;
@@ -66,7 +71,8 @@ export class RequestRouter implements RequestRouterStore, ApiProvider {
      * Loads Service Engines
      */
     private getServiceEngines() {
-        this.ccService.fetchSes()
+        this.ccService
+            .fetchSes()
             .then((ses: ServiceEngineStore[]) => {
                 ses.forEach(se => {
                     const seInstance = new ServiceEngine(
@@ -94,6 +100,18 @@ export class RequestRouter implements RequestRouterStore, ApiProvider {
         this.serviceEngines = [];
     }
 
+    private sendRequestCb(httpEvent: httpEvent, session: session) {
+        const se = this.serviceEngines.find((se: ServiceEngine) => {
+            return se.ip == session.dst_ip && se.port == session.dst_port;
+        });
+        if (se) {
+            se.sendRequest(httpEvent, session);
+        } else {
+            throw new Error(
+                'Service engine returned form Controller, however not found in local DB. Serious!'
+            );
+        }
+    }
 
     /**
      * Starts observing events
@@ -101,7 +119,7 @@ export class RequestRouter implements RequestRouterStore, ApiProvider {
     private observeObservers() {
         /**
          * Once controller connector gets connected and req router is not registered, Register
-         * 
+         *
          * If Controller connector gets disconnected, deregister request router, set lastFetched to null and kill all SEs.
          */
         this.ccService.ccEvents.subscribe((data: ControllerConnectorStore) => {
@@ -117,96 +135,88 @@ export class RequestRouter implements RequestRouterStore, ApiProvider {
 
         /**
          * Subscribe to HTTP events
-         * 
+         *
          * If incoming HTTP request occures, get matching session from Controller and send request via SE if SE found.
          */
         this.httpServer.httpEventSubject.subscribe((httpEvent: httpEvent) => {
-            this.ccService
-                .getMatchingSess(
-                    httpEvent.req.socket.remoteAddress,
-                    httpEvent.req.socket.remotePort,
-                    httpEvent.host,
-                    httpEvent.port
-                )
-                .then((session: session) => {
-                    const se = this.serviceEngines.find((se: ServiceEngine) => {
-                        return (se.ip == session.dst_ip && se.port == session.dst_port)
-                    })
-                    if (se) {
-                        se.sendRequest(httpEvent, session);
-                    } else {
-                        throw new Error('Service engine returned form Controller, however not found in local DB. Serious!');
-                    }
-                })
-                .catch((err) => {
-                    this.logger.error('Error occured when getting the matching session ' + err);
-                    this.logger.error(httpEvent.req.socket.remoteAddress);
-                    this.logger.error(httpEvent.req.socket.remotePort);
-                })
+
+            from(this.ccService.getMatchingSess(
+                httpEvent.req.socket.remoteAddress,
+                httpEvent.req.socket.remotePort,
+                httpEvent.host,
+                httpEvent.port
+            )).pipe(
+                retryWhen(error => error.pipe(
+                    delay(config.get('request_router.get_matching_sess_retry_delay')),
+                    take(config.get('request_router.get_matching_sess_retries'))
+                ))
+            ).subscribe((session: session) => {
+                this.sendRequestCb(httpEvent, session);
+            })
         });
     }
 
     /**
      * Returns list of established sessions from service engines
-     * @param req 
-     * @param res 
+     * @param req
+     * @param res
      */
     getSessions(req: express.Request, res: express.Response) {
         this.logger.debug('API: requesting sessions from RR to SEs');
-        const sessions = this.serviceEngines.map((se) => {
+        const sessions = this.serviceEngines.map(se => {
             return {
-                'se_ip': se.ip,
-                'se_port': se.port,
-                'se_name': se.name,
-                'sessions': se.tcpsessions.map((tcp) => {
+                se_ip: se.ip,
+                se_port: se.port,
+                se_name: se.name,
+                sessions: se.tcpsessions.map(tcp => {
                     return {
-                        'src_ip': tcp.localAddress,
-                        'src_port': tcp.localPort,
-                        'dst_ip': tcp.remoteAddress,
-                        'dst_port': tcp.remotePort                    
-                    }
+                        src_ip: tcp.localAddress,
+                        src_port: tcp.localPort,
+                        dst_ip: tcp.remoteAddress,
+                        dst_port: tcp.remotePort
+                    };
                 })
-            }
+            };
         });
 
         res.send({
-            'sessions': sessions
+            sessions: sessions
         });
     }
 
     getAssets(req: express.Request, res: express.Response) {
         this.logger.debug('API: requesting Assets');
         const data = {
-            'ip': this.ip,
-            'port': this.port,
-            'name': this.name,
-            'domain': this.domain,
-            'ses': this.serviceEngines.map((se) => {
-                return  {
-                    'ip': se.ip,
-                    'port': se.port,
-                    'name': se.name,
-                    'domain': se.domain
-                }
+            ip: this.ip,
+            port: this.port,
+            name: this.name,
+            domain: this.domain,
+            ses: this.serviceEngines.map(se => {
+                return {
+                    ip: se.ip,
+                    port: se.port,
+                    name: se.name,
+                    domain: se.domain
+                };
             })
-        }
+        };
 
         res.send({
-            'rr': data
+            rr: data
         });
     }
 
     /**
      * Returns list of sessons from the Controller
-     * @param req 
-     * @param res 
+     * @param req
+     * @param res
      */
     async getCntSessions(req: express.Request, res: express.Response) {
         this.logger.debug('API: requesting CNT sessions');
         try {
             const sessions = await this.ccService.getAllSessions();
             return res.send(sessions);
-        } catch(err) {
+        } catch (err) {
             this.logger.error('Could not get sessions from controller');
             this.logger.error(err);
         }
@@ -217,7 +227,7 @@ export class RequestRouter implements RequestRouterStore, ApiProvider {
         try {
             const topology = await this.ccService.getTopology();
             return res.send(topology);
-        } catch(err) {
+        } catch (err) {
             this.logger.error('Failed to retrieve topology from controller');
             this.logger.error(err);
         }
